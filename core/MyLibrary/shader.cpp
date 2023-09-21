b0b0e37d9245677aaf7b392a51e28d40b00b927f
Alex <alexander.bianca@mymail.champlain.edu>
2023-09-21 18:09:11 -0400

    Committing new files

---
+++ core/MyLibrary/shader.cpp
@@ -0,0 +1,86 @@
+#include "shader.h"
+#include "C:/Users/alexander.bianca/Desktop/ABianca-gpr200-fa2023-templates/core/ew/external/glad.h"
+
+namespace MyLibrary {
+	std::string loadShaderSourceFromFile(const std::string& filePath) {
+		std::ifstream fstream(filePath);
+		if (!fstream.is_open()) {
+			printf("Failed to load file %s", filePath);
+			return {};
+		}
+		std::stringstream buffer;
+		buffer << fstream.rdbuf();
+		return buffer.str();
+	}
+
+	unsigned int createShader(GLenum shaderType, const char* sourceCode) {
+		//Create a new vertex shader object
+		unsigned int shader = glCreateShader(shaderType);
+		//Supply the shader object with source code
+		glShaderSource(shader, 1, &sourceCode, NULL);
+		//Compile the shader object
+		glCompileShader(shader);
+		int success;
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+		if (!success) {
+			//512 is an arbitrary length, but should be plenty of characters for our error message.
+			char infoLog[512];
+			glGetShaderInfoLog(shader, 512, NULL, infoLog);
+			printf("Failed to compile shader: %s", infoLog);
+		}
+		return shader;
+	}
+
+	unsigned int createShaderProgram(const char* vertexShaderSource, const char* fragmentShaderSource) {
+		unsigned int vertexShader = createShader(GL_VERTEX_SHADER, vertexShaderSource);
+		unsigned int fragmentShader = createShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
+
+		unsigned int shaderProgram = glCreateProgram();
+		//Attach each stage
+		glAttachShader(shaderProgram, vertexShader);
+		glAttachShader(shaderProgram, fragmentShader);
+		//Link all the stages together
+		glLinkProgram(shaderProgram);
+		int success;
+		glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+		if (!success) {
+			char infoLog[512];
+			glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+			printf("Failed to link shader program: %s", infoLog);
+		}
+		//The linked program now contains our compiled code, so we can delete these intermediate objects
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
+		return shaderProgram;
+	}
+
+	Shader::Shader(const std::string& vertextShader, const std::string& fragmentShader) {
+		std::string vertexShaderSource = loadShaderSourceFromFile(vertextShader.c_str());
+		std::string fragmentShaderSource = loadShaderSourceFromFile(fragmentShader.c_str());
+		m_id = createShaderProgram(vertexShaderSource.c_str(), fragmentShaderSource.c_str());
+	}
+
+	void Shader::use() {
+		glUseProgram(m_id);
+	}
+
+	void Shader::setInt(const std::string& name, int v) const{
+		glUniform1i(glGetUniformLocation(m_id, name.c_str()), v);
+	}
+	void Shader::setFloat(const std::string& name, float v) const{
+		glUniform1f(glGetUniformLocation(m_id, name.c_str()), v);
+	}
+
+	void setVec2(const std::string& name, float x, float y) const {
+		glUniform2f(GLint location, GLfloat v0, GLfloat v1);
+	}
+
+	void setVec3(const std::string& name, float x, float y, float z) const {
+		glUniform3f(GLint location, GLfloat v0, GLfloat v1, GLfloat v2);
+	}
+
+	void setVec4(const std::string& name, float x, float y, float z, float w) const {
+		glUniform4f(GLint location, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3);
+	}
+
+}
