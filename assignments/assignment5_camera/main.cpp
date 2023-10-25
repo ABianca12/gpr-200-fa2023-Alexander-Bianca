ea07bdaf3f4a8d6179f54328263ec87e2f2ccf6f
Alex <alexandertbianca@gmail.com>
2023-10-24 21:42:03 -0400

    Finished assignment 5

--- assignments/assignment5_camera/main.cpp
+++ assignments/assignment5_camera/main.cpp
@@ -14,7 +14,7 @@
 #include <MyLibrary/camera.h>
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
-void moveCamera(GLFWwindow* window, MyLibrary::Camera* camera, MyLibrary::CameraControls* controls);
+void moveCamera(GLFWwindow* window, MyLibrary::Camera* camera, MyLibrary::CameraControls* controls, float deltaTime);
 
 //Projection will account for aspect ratio!
 const int SCREEN_WIDTH = 1080;
@@ -73,14 +73,26 @@ int main() {
 		cubeTransforms[i].position.y = i / (NUM_CUBES / 2) - 0.5;
 	}
 
+	cam.position = { 0, 0, 5 };
+
 	MyLibrary::Camera camera;
 	MyLibrary::CameraControls cameraControls;
 
+	float prevTime = (float)glfwGetTime();
+
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
+
+		float time = (float)glfwGetTime();
+		float deltaTime = time - prevTime;
+		prevTime = time;
+
+		moveCamera(window, &cam, &cameraControls, deltaTime);
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		//Clear both color buffer AND depth buffer
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		cam.aspectRatio = (float)resolution[0] / resolution[1];
 
 		//Set uniforms
 		shader.use();
@@ -113,6 +125,7 @@ int main() {
 				}
 				ImGui::PopID();
 			}
+
 			if (ImGui::CollapsingHeader("Camera"))
 			{
 				ImGui::DragFloat3("Camera Position", &cam.position.x, 0.01f);
@@ -123,8 +136,29 @@ int main() {
 					ImGui::DragFloat("Orhtographic Frustum Height", &cam.orthoSize);
 				}
 				else
+				{
 					ImGui::DragFloat("FOV", &cam.fov, 0.1f);
+					ImGui::DragFloat("Near Plane", &cam.nearPlane, 0.1f);
+					ImGui::DragFloat("Far Plane", &cam.farPlane, 0.1f);
+				}
+					
 			}
+
+			if (ImGui::CollapsingHeader("Camera Controller"))
+			{
+				ImGui::Text("Yaw: %.0f", cameraControls.yaw);
+				ImGui::Text("Pitch : %.0f", cameraControls.pitch);
+				ImGui::DragFloat("Move Speed", &cameraControls.moveSpeed, 0.1f);
+				ImGui::DragFloat("Sensitivity", &cameraControls.mouseSensitivity);
+				ImGui::Checkbox("Invert Y", &cameraControls.invertY);
+			}
+
+			if (ImGui::Button("Reset"))
+			{
+				cam.ResetCam();
+				cameraControls.ResetCameraControls();
+			}
+
 			ImGui::End();
 			
 			ImGui::Render();
@@ -136,29 +170,86 @@ int main() {
 	printf("Shutting down...");
 }
 
-void moveCamera(GLFWwindow* window, MyLibrary::Camera* camera, MyLibrary::CameraControls* controls)
+void moveCamera(GLFWwindow* window, MyLibrary::Camera* camera, MyLibrary::CameraControls* controls, float deltaTime)
 {
 	if (!glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT))
 	{
 		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 		controls->firstMouse = true;
-		return;
 	}
-
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-	double mouseX, mouseY;
-	glfwGetCursorPos(window, &mouseX, &mouseY);
-
-	if (controls->firstMouse)
+	else
 	{
-		controls->firstMouse = false;
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+		double mouseX, mouseY;
+		glfwGetCursorPos(window, &mouseX, &mouseY);
+
+		if (controls->firstMouse)
+		{
+			controls->firstMouse = false;
+			controls->prevMouseX = mouseX;
+			controls->prevMouseY = mouseY;
+		}
+
+		double mouseXDelta = mouseX - controls->prevMouseX;
+		double mouseYDelta = mouseY - controls->prevMouseY;
+
+		if (!cameraControl.invertY)
+		{
+			mouseYDelta *= -1;
+		}
+
+		controls->yaw += mouseXDelta * controls->mouseSensitivity * deltaTime;
+		controls->pitch += mouseYDelta * controls->mouseSensitivity * deltaTime;
+
+		if (controls->pitch > 89)
+		{
+			controls->pitch = 89;
+		}
+		else if (controls->pitch < -89)
+		{
+			controls->pitch = -89;
+		}
 		controls->prevMouseX = mouseX;
 		controls->prevMouseY = mouseY;
 	}
 
-	controls->prevMouseX = mouseX;
-	controls->prevMouseY = mouseY;
+	double yawRad = controls->yaw * ew::DEG2RAD;
+	double pitchRad = controls->pitch * ew::DEG2RAD;
+	ew::Vec3 forward = ew::Vec3(sin(yawRad) * cos(pitchRad), sin(pitchRad), -cos(yawRad) * cos(pitchRad));
+	ew::Vec3 right = ew::Normalize(ew::Cross(forward, ew::Vec3(0, 1, 0)));
+	ew::Vec3 up = ew::Normalize(ew::Cross(right, forward));
+
+	if (glfwGetKey(window, GLFW_KEY_W))
+	{
+		camera->position += forward * controls->moveSpeed * deltaTime;
+	}
+		
+	if (glfwGetKey(window, GLFW_KEY_S))
+	{
+		camera->position += -1 * forward * controls->moveSpeed * deltaTime;
+	}
+		
+	if (glfwGetKey(window, GLFW_KEY_A))
+	{
+		camera->position += -1 * right * controls->moveSpeed * deltaTime;
+	}
+		
+	if (glfwGetKey(window, GLFW_KEY_D))
+	{
+		camera->position += right * controls->moveSpeed * deltaTime;
+	}
+		
+	if (glfwGetKey(window, GLFW_KEY_Q))
+	{
+		camera->position += -1 * up * controls->moveSpeed * deltaTime;
+	}
+		
+	if (glfwGetKey(window, GLFW_KEY_E))
+	{
+		camera->position += up * controls->moveSpeed * deltaTime;
+	}
+	camera->target = camera->position + forward;
 }
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height)
--- core/MyLibrary/camera.h
+++ core/MyLibrary/camera.h
@@ -8,7 +8,7 @@ namespace MyLibrary
 	{
 		ew::Vec3 position = (0, 0, 5);
 		ew::Vec3 target = (0, 0, 0);
-		float fov = 90;
+		float fov = 70;
 		float aspectRatio = 4/3;
 		float nearPlane = 0.1;
 		float farPlane = 100.0;
@@ -16,14 +16,41 @@ namespace MyLibrary
 		float orthoSize = 6;
 		ew::Mat4 ViewMatrix();
 		ew::Mat4 ProjectionMatrix();
+
+		const ew::Vec3 INITAL_POS = position;
+		const ew::Vec3 INITAL_TARGET = target;
+		const float INITAL_FOV = fov;
+		const float INITAL_NEAR_PLANE = nearPlane;
+		const float INITAL_FAR_PLANE = farPlane;
+		const float INITAL_ORTHO_SIZE = orthoSize;
+
+		void ResetCam()
+		{
+			position = INITAL_POS;
+			target = INITAL_TARGET;
+			fov = INITAL_FOV;
+			nearPlane = INITAL_NEAR_PLANE;
+			farPlane = INITAL_FAR_PLANE;
+			orthoSize = INITAL_ORTHO_SIZE;
+		}
 	};
 
 	struct CameraControls
 	{
 		double prevMouseX, prevMouseY;
 		float yaw = 0, pitch = 0;
-		float mouseSensitivity = 0.1;
+		float mouseSensitivity = 5.0;
 		bool firstMouse = true;
 		float moveSpeed = 5.0f;
+		bool invertY = false;
+
+		const float INITAL_YAW = yaw;
+		const float INITAL_PITCH = pitch;
+
+		void ResetCameraControls()
+		{
+			yaw = INITAL_YAW;
+			pitch = INITAL_PITCH;
+		}
 	};
 }
