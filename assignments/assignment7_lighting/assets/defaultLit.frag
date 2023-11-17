378d2a2d410f96eb4582be75788f0153f29b58ae
Alex <alexandertbianca@gmail.com>
2023-11-16 20:30:25 -0500

    Completed

--- assignments/assignment7_lighting/assets/defaultLit.frag
+++ assignments/assignment7_lighting/assets/defaultLit.frag
@@ -3,10 +3,68 @@ out vec4 FragColor;
 
 in Surface{
 	vec2 UV;
+	vec3 WorldPosition;
+	vec3 WorldNormal;
 }fs_in;
+
+struct Light
+{
+	vec3 position;
+	vec3 color;
+};
+
+#define MAX_NUM_OF_LIGHTS 4
+uniform Light _LightsArray[MAX_NUM_OF_LIGHTS];
+
+uniform float _ambient;
+uniform float _diffuse;
+uniform float _specular;
+uniform float _shine;
+
+vec3 reflectionVec;
+uniform vec3 _cameraPos;
+vec3 viewingAngle;
+uniform bool _blinnPhong = false;
 
 uniform sampler2D _Texture;
 
+float diffuseFactor;
+float specularFactor;
+
 void main(){
 	FragColor = texture(_Texture,fs_in.UV);
+
+	vec3 normal = normalize(fs_in.WorldNormal);
+	vec3 baseFragRGB = FragColor.rgb;
+	FragColor = vec4(0, 0, 0, 0);
+
+	for (int i = 0; i < MAX_NUM_OF_LIGHTS; i++)
+	{
+		vec3 lightDir = normalize(_LightsArray[i].position - fs_in.WorldPosition);
+
+		diffuseFactor = max(dot(normal, lightDir), 0) * _diffuse;
+
+		viewingAngle = normalize(_cameraPos - fs_in.WorldPosition);
+
+		if (_blinnPhong)
+		{
+			vec3 h = normalize(lightDir + viewingAngle);
+			specularFactor = pow(max(dot(h, normal), 0), _shine);
+		}
+		else
+		{
+			reflectionVec = 2 * dot(lightDir, normal) * normal - lightDir;
+			specularFactor = pow(max(dot(reflectionVec, viewingAngle), 0), _shine);
+		}
+
+		specularFactor *= _specular;
+
+		vec3 ambientColor = _LightsArray[i].color * _ambient;
+		vec3 diffuseColor = _LightsArray[i].color * diffuseFactor;
+		vec3 specularColor = _LightsArray[i].color * specularFactor;
+
+		FragColor += vec4(baseFragRGB * (ambientColor + diffuseColor + specularColor), 1);
+	}
+
+	normalize(FragColor);
 }
--- assignments/assignment7_lighting/main.cpp
+++ assignments/assignment7_lighting/main.cpp
@@ -15,6 +15,8 @@
 #include <ew/camera.h>
 #include <ew/cameraController.h>
 
+using namespace std;
+
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 void resetCamera(ew::Camera& camera, ew::CameraController& cameraController);
 
@@ -26,6 +28,29 @@ ew::Vec3 bgColor = ew::Vec3(0.1f);
 
 ew::Camera camera;
 ew::CameraController cameraController;
+
+struct Light
+{
+	ew::Vec3 color = { 1, 1, 1 };
+	ew::Transform transform;
+};
+
+const int MAX_NUM_OF_LIGHTS = 4;
+int numLights = MAX_NUM_OF_LIGHTS;
+
+Light lightsArray[MAX_NUM_OF_LIGHTS];
+
+struct Material
+{
+	float ambient = 0.1;
+	float diffuse = 0.25;
+	float specular = 1.0;
+	float shine = 128;
+};
+
+Material material;
+
+bool blinnPhong = true;
 
 int main() {
 	printf("Initializing...");
@@ -59,13 +84,15 @@ int main() {
 	glEnable(GL_DEPTH_TEST);
 
 	ew::Shader shader("assets/defaultLit.vert", "assets/defaultLit.frag");
-	unsigned int brickTexture = ew::loadTexture("assets/brick_color.jpg",GL_REPEAT,GL_LINEAR);
+	unsigned int brickTexture = ew::loadTexture("assets/brick_color.jpg", GL_REPEAT, GL_LINEAR);
+	ew::Shader unlitShader("assets/unlit.vert", "assets/unlit.frag");
 
 	//Create cube
 	ew::Mesh cubeMesh(ew::createCube(1.0f));
 	ew::Mesh planeMesh(ew::createPlane(5.0f, 5.0f, 10));
 	ew::Mesh sphereMesh(ew::createSphere(0.5f, 64));
 	ew::Mesh cylinderMesh(ew::createCylinder(0.5f, 1.0f, 32));
+	ew::Mesh lightSphere(ew::createSphere(0.1f, 16));
 
 	//Initialize transforms
 	ew::Transform cubeTransform;
@@ -77,6 +104,15 @@ int main() {
 	cylinderTransform.position = ew::Vec3(1.5f, 0.0f, 0.0f);
 
 	resetCamera(camera,cameraController);
+
+	lightsArray[0].transform.position = { 2, 1, 2 };
+	lightsArray[0].color = { 1,0,0 };
+	lightsArray[1].transform.position = { 2, 1, -2 };
+	lightsArray[1].color = { 0,1,0 };
+	lightsArray[2].transform.position = { -2, 1, -2 };
+	lightsArray[2].color = { 0,0,1 };
+	lightsArray[3].transform.position = { -2, 1, 2 };
+	lightsArray[3].color = { 1,1,0 };
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -98,6 +134,26 @@ int main() {
 		shader.setInt("_Texture", 0);
 		shader.setMat4("_ViewProjection", camera.ProjectionMatrix() * camera.ViewMatrix());
 
+		for (int i = 0; i < MAX_NUM_OF_LIGHTS; i++)
+		{
+			if (i < numLights)
+			{
+				shader.setVec3(("_LightsArray[" + to_string(i) + "].position"), lightsArray[i].transform.position);
+				shader.setVec3(("_LightsArray[" + to_string(i) + "].color"), lightsArray[i].color);
+			}
+			else
+			{
+				shader.setVec3(("_LightsArray[" + std::to_string(i) + "].color"), ew::Vec3(0, 0, 0));
+			}
+		}
+
+		shader.setFloat("_ambient", material.ambient);
+		shader.setFloat("_diffuse", material.diffuse);
+		shader.setFloat("_shine", material.shine);
+		shader.setFloat("_specular", material.specular);
+		shader.setVec3("_cameraPos", camera.position);
+		shader.setBool("_blinnPhong", blinnPhong);
+
 		//Draw shapes
 		shader.setMat4("_Model", cubeTransform.getModelMatrix());
 		cubeMesh.draw();
@@ -112,6 +168,15 @@ int main() {
 		cylinderMesh.draw();
 
 		//TODO: Render point lights
+		unlitShader.use();
+		unlitShader.setMat4("_ViewProjection", camera.ProjectionMatrix() * camera.ViewMatrix());
+
+		for (int i = 0; i < numLights; i++)
+		{
+			unlitShader.setVec3("_Color", lightsArray[i].color);
+			unlitShader.setMat4("_Model", lightsArray[i].transform.getModelMatrix());
+			lightSphere.draw();
+		}
 
 		//Render UI
 		{
@@ -140,6 +205,29 @@ int main() {
 			}
 
 			ImGui::ColorEdit3("BG color", &bgColor.x);
+
+			ImGui::SliderInt("Number of lights", &numLights, 0, MAX_NUM_OF_LIGHTS);
+
+			for (int i = 0; i < numLights; i++)
+			{
+				ImGui::PushID(i);
+				if (ImGui::CollapsingHeader("Light"))
+				{
+					ImGui::DragFloat3("Position", &lightsArray[i].transform.position.x, 0.1);
+					ImGui::ColorEdit3("Color", &lightsArray[i].color.x);
+				}
+				ImGui::PopID();
+			}
+
+			if (ImGui::CollapsingHeader("Lighting Settings"))
+			{
+				ImGui::SliderFloat("Ambient", &material.ambient, 0.0f, 1.0f);
+				ImGui::SliderFloat("Diffuse", &material.diffuse, 0.0f, 1.0f);
+				ImGui::SliderFloat("Shine", &material.shine, 2.0f, 1024.0f);
+				ImGui::SliderFloat("Specular", &material.specular, 0.0f, 1.0f);
+				ImGui::Checkbox("Blinn-Phong", &blinnPhong);
+			}
+
 			ImGui::End();
 			
 			ImGui::Render();
